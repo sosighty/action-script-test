ffea5322caa52fc9a3ea435d879654747d4f4b67
Naïm Boughattas <boughattas.naim@gmail.com>
2024-01-11 15:59:53 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ export async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
     const url = core.getInput('storybook_url')
     core.debug(url)
-    core.debug(key)
+    core.info(key)
 
     // const listUsersResponse = await notion.users.list({})
     // core.debug(listUsersResponse.results[0].name ?? 'No name')
